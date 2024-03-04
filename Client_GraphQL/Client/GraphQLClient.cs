aafbcac7abac226a37032ee2439146258d8ecb5a
luisfbcudj94 <alejandroabril98@gmail.com>
2024-03-04 16:01:05 -0500

    feat: Create client to API

---
+++ Client_GraphQL/Client/GraphQLClient.cs
@@ -0,0 +1,55 @@
+﻿using Client_GraphQL.Models;
+using GraphQL.Client.Abstractions;
+using GraphQL.Client.Http;
+using GraphQL.Client.Serializer.Newtonsoft;
+using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+using static System.Net.WebRequestMethods;
+
+namespace Client_GraphQL.Client
+{
+    public class GraphQLClient
+    {
+        private readonly GraphQLHttpClient _client;
+        private GraphQLHttpClient graphqlHttpClient = new GraphQLHttpClient("https://localhost:7012/graphql", new NewtonsoftJsonSerializer());
+
+        private GraphQLHttpClient graphQLClient = new GraphQLHttpClient("https://localhost:7012/graphql", new NewtonsoftJsonSerializer());
+
+
+        public GraphQLClient()
+        {
+            
+        }
+
+        public async Task<PaginatedList> SendQueryAsync<PaginatedList>(string query)
+        {
+            try
+            {
+                var response = await graphQLClient.SendQueryAsync<PaginatedList>(query);
+
+                if (response.Errors != null)
+                {
+                    foreach (var error in response.Errors)
+                    {
+                        Console.WriteLine($"Error: {error.Message}");
+                    }
+                }
+                else
+                {
+                    var data = response.Data;
+                    Console.WriteLine($"Response data: {data}");
+                }
+
+                return response.Data;
+            }
+            catch (Exception ex)
+            {
+                Console.WriteLine($"Error while sending the query: {ex.Message}");
+                throw;
+            }
+        }
+    }
+}
---
+++ Client_GraphQL/Program.cs
@@ -0,0 +1,33 @@
+﻿using Client_GraphQL.Client;
+using Client_GraphQL.Models;
+
+var query = @"
+        query {
+            publisherCommissions {
+                count
+                payloadComplete
+                maxId
+                records {
+                    commissionId
+                    advertiserName
+                    actionType
+                    saleAmountUsd
+                    orderDiscountUsd
+                    pubCommissionAmountUsd
+                    actionTrackerName
+                    websiteName
+                    aid
+                    postingDate
+                    eventDate
+                    orderId
+                    coupon
+                    isCrossDevice
+                }
+            }
+        }
+        ";
+GraphQLClient client = new GraphQLClient();
+
+var data = await client.SendQueryAsync<dynamic>(query);
+
+Console.ReadLine();
