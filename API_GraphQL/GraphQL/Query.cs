1169f20ac3673ee3536e32b4113c086010127fc1
luisfbcudj94 <alejandroabril98@gmail.com>
2024-03-08 05:39:56 -0500

    feat: Create frontend project

--- API_GraphQL/GraphQL/Query.cs
+++ API_GraphQL/GraphQL/Query.cs
@@ -32,9 +32,9 @@ namespace API_GraphQL.GraphQL
         /// <param name="startDate">Start date of the date range to filter.</param>
         /// <param name="endDate">End date of the date range to filter.</param>
         /// <returns>A paginated list of publisher commissions.</returns>
-        public PaginatedList PublisherCommissions(DateTime startDate, DateTime endDate, Guid? sinceId = null)
+        public PaginatedList PublisherCommissions(DateTime sincePostingDate, DateTime beforePostingDate, Guid? sinceCommissionId = null)
         {
-            var response = _commisionsService.GetCommissionsPaginated(startDate, endDate, sinceId);
+            var response = _commisionsService.GetCommissionsPaginated(sincePostingDate, beforePostingDate, sinceCommissionId);
 
             return response;
         }
