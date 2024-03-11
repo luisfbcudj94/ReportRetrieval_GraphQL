10004bbeef53dfb1b10e640781e58cd68ec337f1
luisfbcudj94 <alejandroabril98@gmail.com>
2024-03-11 08:41:40 -0500

    feat: Implement download csv file

---
+++ FrontEnd_GraphQL/Application/Interfaces/ICSVService.cs
@@ -0,0 +1,9 @@
+﻿using FrontEnd_GraphQL.Application.Models;
+
+namespace FrontEnd_GraphQL.Application.Interfaces
+{
+    public interface ICSVService
+    {
+        byte[] ExportData(List<Commissions> newData);
+    }
+}
