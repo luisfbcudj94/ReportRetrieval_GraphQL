dcea584eb28ddfc553c3e233a27df39cad3c950c
luisfbcudj94 <alejandroabril98@gmail.com>
2024-03-13 11:29:23 -0500

    feat: Add toast - validate dates

--- FrontEnd_GraphQL/Program.cs
+++ FrontEnd_GraphQL/Program.cs
@@ -5,6 +5,8 @@ using FrontEnd_GraphQL.Application.Interfaces;
 using FrontEnd_GraphQL.Application.Services;
 using Microsoft.AspNetCore.Components.Web;
 using Microsoft.AspNetCore.Components.WebAssembly.Hosting;
+using BlazorBootstrap;
+using Blazored.Toast;
 
 var builder = WebAssemblyHostBuilder.CreateDefault(args);
 builder.RootComponents.Add<App>("#app");
@@ -13,6 +15,7 @@ builder.RootComponents.Add<HeadOutlet>("head::after");
 builder.Services.AddScoped(sp => new HttpClient { BaseAddress = new Uri(builder.HostEnvironment.BaseAddress) });
 builder.Services.AddBlazorBootstrap();
 builder.Services.AddBlazorise();
+builder.Services.AddBlazoredToast();
 
 builder.Services.AddScoped<ICommissionsService, CommissionsService>();
 builder.Services.AddScoped<IGraphQLClientService, GraphQLClientService>();
